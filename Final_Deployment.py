88661cab989d1f37eee8efb352cfc31c5dcac058
Tripti Deshpande <92503889+TriptiDeshpande@users.noreply.github.com>
2022-06-08 19:29:26 +0530

    Update Final_Deployment.py

--- Final_Deployment.py
+++ Final_Deployment.py
@@ -13,7 +13,7 @@ st.set_page_config(layout='wide', initial_sidebar_state='expanded',page_icon="sm
 st.title('Text Sentiment Analysis')
 
 st.markdown('Type a sentence in the below text box and choose the desired option in the adjacent menu.')
-side = st.sidebar.selectbox("Select an option below", ("Sentiment", "Name Entity Recognizer"))
+#side = st.sidebar.selectbox("Select an option below", ("Sentiment", "Name Entity Recognizer"))
 Text = st.text_input("Enter the sentence")
 
 def sentiment(text):
@@ -25,16 +25,14 @@ def sentiment(text):
     else:
         return ("Positive with a score of : ", analysis.sentiment.polarity)
 
-def ner(sentence):
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(sentence)
-    ents = [(e.text, e.label_) for e in doc.ents]
-    return ents
+#def ner(sentence):
+    #nlp = spacy.load("en_core_web_sm")
+    #doc = nlp(sentence)
+    #ents = [(e.text, e.label_) for e in doc.ents]
+   # return ents
 def run():
     if side == "Sentiment":
         st.write(sentiment(Text))    
-    if side == "Name Entity Recognizer":
-        st.write(ner(Text))
 if __name__ == '__main__':
     run()
 
